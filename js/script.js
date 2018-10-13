372ad2dab1021cf412bf1136e1633c46ae2d570a
lukasz341 <lukasz341@tlen.pl>
2018-10-13 21:34:51 +0200

    add new animals

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ function Animal(name,sound){
     function playMusic() {
     audio.play();
     button.classList.add(self.name);
+
+   // if audio.stop(){
+    //  button.classList.remove(self.name);
+   // }
   }
 
   function deleteClass(){
@@ -24,16 +28,16 @@ function Animal(name,sound){
   }
   
   var cow =new Animal("cow","sound/cow.mp3");
- // var duck=new Animal("duck","sound/duck.mp3");
+ var duck=new Animal("duck","sound/duck.mp3");
   var chicken=new Animal("chicken","sound/chicken.mp3");
-  var horse=new Animal("horse","sound/cow.mp3");
-  var rooster=new Animal("rooster","sound/chicken.mp3");
-  var pig=new Animal("pig", "sound/chicken.mp3");
-  var farmer=new Animal("farmer", "sound/chicken.mp3");
-  var tractor=new Animal("tractor", "sound/chicken.mp3");
+  var horse=new Animal("horse","sound/horse.mp3");
+  var rooster=new Animal("rooster","sound/rooster.mp3");
+  var pig=new Animal("pig", "sound/pig.mp3");
+  var farmer=new Animal("farmer", "sound/farmer.wav");
+  var tractor=new Animal("tractor", "sound/tractor.mp3");
  
   cow.event();
-  //duck.event();
+  duck.event();
   chicken.event();
   horse.event();
   rooster.event();
